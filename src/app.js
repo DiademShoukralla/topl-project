00ef23cc0469a290188e6ab9ea6762055986e42d
Diadem Shoukralla <diademshoukralla@gmail.com>
2022-06-11 13:49:03 -0400

    Added some boilerplate/starter code.

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
+
+app.use((err, req, res, next) => {
+    res.send({ error: err.message });
+});
+
+module.exports = app;
