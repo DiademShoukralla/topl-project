a446511fbd9ee7b26ad2f3d040cd37622838d7a6
Diadem Shoukralla <diademshoukralla@gmail.com>
2022-06-11 14:10:57 -0400

    Added Husky pre-commit hook to run format code and run tests prior to commiting. Currently trivial tests

---
+++ __tests__/temp.js
@@ -0,0 +1,5 @@
+describe("Placeholder test suite", () => {
+  it("Placeholder trivial test", () => {
+    expect(true).toEqual(true);
+  });
+});
--- src/app.js
+++ src/app.js
@@ -4,9 +4,8 @@ const app = express();
 const morgan = require("morgan");
 app.use(morgan("dev"));
 
-
 app.use((err, req, res, next) => {
-    res.send({ error: err.message });
+  res.send({ error: err.message });
 });
 
 module.exports = app;
